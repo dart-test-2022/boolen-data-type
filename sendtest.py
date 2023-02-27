d01a9b04b3eef6528d5385f6541da5c9875563ea
Diyorbek Jumanov <76900500+djumanov@users.noreply.github.com>
2023-02-27 14:53:03 +0500

    Update sendtest.py

--- sendtest.py
+++ sendtest.py
@@ -9,6 +9,15 @@ url = 'https://codeschoolhomeworkapi.pythonanywhere.com/'
 f = open('data.json', 'r')
 data = json.load(f)
 
+j = 0
+for i in data:
+    i['name'] = i['name'].split('.')[0]
+    data[j] = i
+    j += 1
+
+print('*'*10 + 'data' + '*'*10)
+pprint(data)
+   
 parser = argparse.ArgumentParser()
 parser.add_argument('github', help='github username')
 parser.add_argument('repo', help='repository | homework')
@@ -16,24 +25,30 @@ parser.add_argument('changed_files', help='changed files')
 
 arg = parser.parse_args()
 
-changed_files = [f.split('.')[0] for f in arg.changed_files.split()]
+changed_files = arg.changed_files.split()
+print('*'*10 + 'changed files' + '*'*10)
+print(changed_files)
+
 
 tasks = []
-# comment qildim sababi bitta fileda barcha task qaysi methodsni ishlaganini olib bomadi
-# i = 0
-# while i < len(data):
-#     if data[i]['name'] in changed_files:
-#         tasks.append(data[i])
+i = 0
+while i < len(data):
+    if data[i]['name'] in changed_files:
+        tasks.append(data[i])
 
-#     i += 1
+    i += 1
 
 dct = {
     "github": arg.github,
     "repo": arg.repo,
-    "tasks": data
+    "tasks": tasks
 }
 
 f.close()
+
+print('*'*10 + 'changed files' + '*'*10)
+pprint(dct)
+
 r = requests.post(url+'homework/attempt/', json=dct)
 print(r.status_code)
 
@@ -41,6 +56,11 @@ print(r.status_code)
 corrects = 0
 for answer in data:
     corrects += answer['isSolved']
+    
+    
+print('*'*10 + 'corrcetes' + '*'*10)
+print(changed_files)
+
 
 print('=' * 8 + ' Natijalar: ' + '=' * 8)
     # Print readability report
@@ -55,7 +75,7 @@ for task in data:
 
  
 # Check if all tasks are solved otherwise raise error
-if corrects == len(data):
+if corrects == len(data) and len(data) != 0:
     print('All tasks are solved')
     print('Barcha vazifalar topshirildi')
 else:
